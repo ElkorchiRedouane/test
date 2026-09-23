c2a30b4fe6a6dabb11d761fd9f7fb095ac41f9d6
ElkorchiRedouane <105808468+ElkorchiRedouane@users.noreply.github.com>
2026-09-23 10:19:37 -0400

    Refresh PWA cache for automatic Google Sheet sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="milou-ops-v10";
+const CACHE="milou-ops-v11";
 const APP_SHELL=["/","/manifest.webmanifest","/milou-icon.svg","/milou-icon-maskable.svg"];
 
 self.addEventListener("install",event=>{
